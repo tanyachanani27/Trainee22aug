6b1cf2587ccde67055ea9c17c70ca987e863e7cf
tanyachanani27 <user32@trainux01.jrf11vejvqfuhbwnm0ewggipoa.ax.internal.cloudapp.net>
2024-09-04 09:46:33 +0000

    Updated

---
+++ Arrays/printarray.c
@@ -0,0 +1,35 @@
+/*Write a program to read and print array elements( using index and using address)*/
+
+#include<stdio.h>
+#define MAX 5
+int main()
+{
+int i;
+ int a[MAX];
+ printf("ENTER THE ARRAY ELEMENTS : \n");
+ printf("using index:");
+ for(i=0;i<MAX ;i++)
+ {
+    scanf("%d",&a[i]);
+
+}
+ for(i=0;i<MAX ;i++)
+ {
+    printf("%d\t",a[i]);
+
+}
+printf("\n");
+
+
+
+printf("using address:\n");
+for(i=0;i<MAX;i++)
+{
+  scanf("%d",*(a+1));
+}
+for(i=0;i<MAX;i++)
+ {
+  printf("%d",*(a+1));
+}
+return 0;
+}
